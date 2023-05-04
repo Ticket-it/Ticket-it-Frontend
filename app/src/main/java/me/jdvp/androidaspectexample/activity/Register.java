7695dd278d612e05364fdcd0d28370582a8c6b9c
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2023-05-04 19:31:33 +0300

    Change activity files to .java

---
+++ app/src/main/java/me/jdvp/androidaspectexample/activity/Register.java
@@ -0,0 +1,13 @@
+package me.jdvp.androidaspectexample.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import me.jdvp.androidaspectexample.R;
+
+public class Register extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
