4b3e3fd257c1d477b04459e908ce7482691640bc
Abouraya11 <youssefabouraya7@gmail.com>
2023-05-07 19:57:59 +0300

    Implementing Login Functionality

--- app/src/main/java/me/jdvp/androidaspectexample/Interface/LoginService.java
+++ app/src/main/java/me/jdvp/androidaspectexample/Interface/LoginService.java
@@ -2,6 +2,8 @@ package me.jdvp.androidaspectexample.Interface;
 
 import me.jdvp.androidaspectexample.model.user.CreateUserRequest;
 import me.jdvp.androidaspectexample.model.user.CreateUserResponse;
+import me.jdvp.androidaspectexample.model.user.LoginRequest;
+import me.jdvp.androidaspectexample.model.user.LoginResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
@@ -10,5 +12,8 @@ public interface LoginService {
 
     @POST("create")
     Call<CreateUserResponse> createUser(@Body CreateUserRequest user);
+
+    @POST("login")
+    Call<LoginResponse> logIn(@Body LoginRequest user);
 
 }
--- app/src/main/java/me/jdvp/androidaspectexample/config/ApiUrls.java
+++ app/src/main/java/me/jdvp/androidaspectexample/config/ApiUrls.java
@@ -1,6 +1,14 @@
 package me.jdvp.androidaspectexample.config;
 
 public class ApiUrls {
-    public static final String BASE_URL = "http://192.168.100.19:3003/";
-    public static final String LOGIN_URL = BASE_URL + "api/account/";
+    // Mazen
+    // public static final String IP_ADDRESS="192.168.100.19";
+
+    // Abouraya
+    public static final String IP_ADDRESS="192.168.1.2";
+
+    // Login Port
+    public static final String LOGIN_PORT=":3003/";
+
+    public static final String LOGIN_URL = "http://"+IP_ADDRESS+LOGIN_PORT+"api/account/";
 }
