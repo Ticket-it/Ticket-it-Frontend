0df2fe4a83b226044be5cee994a80722ea233c8f
Mostafa Hesham <mostafah2000@live.com>
2023-05-14 22:28:07 +0300

    Ticket details UI for Admins and Agents implemented

---
+++ app/src/main/java/me/jdvp/androidaspectexample/activity/admin/TicketDetailsAdmin.java
@@ -0,0 +1,16 @@
+package me.jdvp.androidaspectexample.activity.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.jdvp.androidaspectexample.R;
+
+public class TicketDetailsAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_details_admin);
+    }
+}
