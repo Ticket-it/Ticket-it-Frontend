9133baf4ff4289db3c58bbcc2308c13684fefef4
omarr18 <omarhussein58.oh@gmail.com>
2023-05-14 23:19:52 +0300

    Event history page

---
+++ app/src/main/java/me/jdvp/androidaspectexample/Adapters/EventHistoryAdapter.java
@@ -0,0 +1,77 @@
+package me.jdvp.androidaspectexample.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.jdvp.androidaspectexample.APIModel.Events.EventResponse;
+import me.jdvp.androidaspectexample.Models.EventHistoryModel;
+import me.jdvp.androidaspectexample.R;
+
+public class EventHistoryAdapter extends RecyclerView.Adapter<EventHistoryAdapter.MyHolder> {
+    private Context myContext ;
+    private List<EventHistoryModel> myData;
+
+    public EventHistoryAdapter(Context myContext, ArrayList<EventHistoryModel> myData) {
+        this.myContext = myContext;
+        this.myData = myData;
+    }
+
+    @NonNull
+    @Override
+    public EventHistoryAdapter.MyHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view ;
+        LayoutInflater layoutInflater = LayoutInflater.from(myContext);
+        view = layoutInflater.inflate(R.layout.event_history_card,viewGroup,false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventHistoryAdapter.MyHolder myHolder, int position) {
+        myHolder.image.setImageResource(R.drawable.image_event);
+        myHolder.title.setText(myData.get(myHolder.getAdapterPosition()).getTitle());
+        myHolder.address.setText(myData.get(myHolder.getAdapterPosition()).getLocation());
+        myHolder.price.setText(String.valueOf(myData.get(myHolder.getAdapterPosition()).getPrice()));
+        myHolder.status.setText(myData.get(myHolder.getAdapterPosition()).getStatus());
+    }
+
+    @Override
+    public int getItemCount() {
+        try {
+            return myData.size();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+        TextView address;
+        TextView price;
+
+        ImageView image;
+
+        TextView status;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.event_title);
+            address = itemView.findViewById(R.id.event_address);
+            price = itemView.findViewById(R.id.event_price);
+            image = itemView.findViewById(R.id.event_image);
+            status = itemView.findViewById(R.id.event_status);
+
+        }
+    }
+}
--- app/src/main/java/me/jdvp/androidaspectexample/activity/user/BookingFragment.java
+++ app/src/main/java/me/jdvp/androidaspectexample/activity/user/BookingFragment.java
@@ -1,23 +1,55 @@
 package me.jdvp.androidaspectexample.activity.user;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import java.util.ArrayList;
+
+import me.jdvp.androidaspectexample.Adapters.AdminEventAdapter;
+import me.jdvp.androidaspectexample.Adapters.AdminEventTypeAdapter;
+import me.jdvp.androidaspectexample.Adapters.EventHistoryAdapter;
+import me.jdvp.androidaspectexample.Models.EventHistoryModel;
+import me.jdvp.androidaspectexample.Models.EventModel;
+import me.jdvp.androidaspectexample.Models.EventTypeModel;
 import me.jdvp.androidaspectexample.R;
 
 
 public class BookingFragment extends Fragment {
 
-
-
+    RecyclerView eventsHistoryRecyclerView;
+    EventHistoryAdapter eventHistoryAdapter;
+    ArrayList<EventHistoryModel> events = new ArrayList<>();
+    @SuppressLint("MissingInflatedId")
     @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_booking, container, false);
+        View view = inflater.inflate(R.layout.fragment_booking, container, false);
+
+
+        events.add(new EventHistoryModel("Amr Diab’s New Year", "Family Park, Fifth Settlement", "@drawable/event_img", 750, "Amr Diab is one of the most eminent Arabic pop stars in the Arabic world and a Guinness World Record Holder, Best Selling Middle Eastern Artist, 7 times winner of World Music Awards, 5 Platinum Record Awards & 6 African Music Awards.", "28/05/2023", "07:30 PM", "Cairo, Egypt","confirmed"));
+        events.add(new EventHistoryModel("Amr Diab’s New Year", "Family Park, Fifth Settlement", "@drawable/event_img", 750, "Amr Diab is one of the most eminent Arabic pop stars in the Arabic world and a Guinness World Record Holder, Best Selling Middle Eastern Artist, 7 times winner of World Music Awards, 5 Platinum Record Awards & 6 African Music Awards.", "28/05/2023", "07:30 PM", "Cairo, Egypt","confirmed"));
+        events.add(new EventHistoryModel("Amr Diab’s New Year", "Family Park, Fifth Settlement", "@drawable/event_img", 750, "Amr Diab is one of the most eminent Arabic pop stars in the Arabic world and a Guinness World Record Holder, Best Selling Middle Eastern Artist, 7 times winner of World Music Awards, 5 Platinum Record Awards & 6 African Music Awards.", "28/05/2023", "07:30 PM", "Cairo, Egypt","confirmed"));
+        events.add(new EventHistoryModel("Amr Diab’s New Year", "Family Park, Fifth Settlement", "@drawable/event_img", 750, "Amr Diab is one of the most eminent Arabic pop stars in the Arabic world and a Guinness World Record Holder, Best Selling Middle Eastern Artist, 7 times winner of World Music Awards, 5 Platinum Record Awards & 6 African Music Awards.", "28/05/2023", "07:30 PM", "Cairo, Egypt","confirmed"));
+        events.add(new EventHistoryModel("Amr Diab’s New Year", "Family Park, Fifth Settlement", "@drawable/event_img", 750, "Amr Diab is one of the most eminent Arabic pop stars in the Arabic world and a Guinness World Record Holder, Best Selling Middle Eastern Artist, 7 times winner of World Music Awards, 5 Platinum Record Awards & 6 African Music Awards.", "28/05/2023", "07:30 PM", "Cairo, Egypt","confirmed"));
+
+
+
+        eventsHistoryRecyclerView = (RecyclerView) view.findViewById(R.id.events_history_recycler_view);
+        eventHistoryAdapter = new EventHistoryAdapter(getActivity(), events);
+        eventsHistoryRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 1));
+        eventsHistoryRecyclerView.setAdapter(eventHistoryAdapter);
+
+
+        return view;
     }
 }
