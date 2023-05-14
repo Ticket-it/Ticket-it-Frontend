8064719967c02ba08a9fc6e3cd6709ceb23590ae
Mostafa Hesham <mostafah2000@live.com>
2023-05-14 22:43:26 +0300

    Event Attendants UI implemented

---
+++ app/src/main/java/me/jdvp/androidaspectexample/activity/agent/EventAttendants.java
@@ -0,0 +1,16 @@
+package me.jdvp.androidaspectexample.activity.agent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.jdvp.androidaspectexample.R;
+
+public class EventAttendants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_attendants);
+    }
+}
